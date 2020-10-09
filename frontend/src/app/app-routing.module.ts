dce1fd4e33d04f023638898c675bdb68606b1dac
Dieferson Romanoski <diefe-ki@outlook.com>
2020-10-09 10:29:35 -0300

    Home and products routes added

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
+import { ProductCrudComponent } from './views/product-crud/product-crud.component';
+import { HomeComponent } from './views/home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "", component: HomeComponent
+  },
+  {
+    path: "products", component: ProductCrudComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
