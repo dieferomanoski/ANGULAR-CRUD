3c8bd87c099b185a3783bc00d9cb34c4cfbd6efb
Dieferson Romanoski <diefe-ki@outlook.com>
2020-10-09 10:08:03 -0300

    Component home created, and update module with material card comp.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -11,14 +11,19 @@ import { NavComponent } from './components/template/nav/nav.component'
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav'
-import { MatListModule } from '@angular/material/list'
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+
+
+import { HomeComponent } from './views/home/home.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -26,7 +31,8 @@ import { MatListModule } from '@angular/material/list'
     BrowserAnimationsModule,
     MatToolbarModule,
     MatSidenavModule,
-    MatListModule
+    MatListModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
