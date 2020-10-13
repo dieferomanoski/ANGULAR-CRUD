17e929f3837b4e83dfc59e51ec440bdc19a3a6e0
Dieferson Romanoski <diefe-ki@outlook.com>
2020-10-13 11:06:50 -0300

    some updates on the view

--- frontend/src/app/components/product/product-read/product-read.component.ts
+++ frontend/src/app/components/product/product-read/product-read.component.ts
@@ -1,6 +1,10 @@
+
 import { ProductService } from './../product.service';
 import { Product } from './../product.model';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTable } from '@angular/material/table';
 
 @Component({
   selector: 'product-read',
@@ -12,13 +16,17 @@ export class ProductReadComponent implements OnInit {
   products: Product[];
   displayedColumns = ['id', 'name', 'price', 'action']
 
+
   constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
+
+
     this.productService.read().subscribe(products => {
       this.products = products;
       console.log(products)
     })
   }
+
 
 }
